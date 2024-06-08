beccff588d65ed6dc227a315aa3aeb9aafc96a8c
Thais-Westphal <thais.westphal12@gmail.com>
2024-06-07 21:55:01 -0300

    Exemplos de operadores aritmeticos

---
+++ tipos-variaveis/src/TiposOperadores.java
@@ -0,0 +1,21 @@
+public class TiposOperadores 
+{
+    public static void main(String[] args) throws Exception 
+    {
+          double soma = 10.5 + 15.7;
+          int subtracao = 113 - 25;
+          int multiplicacao = 20 * 7;
+          int divisao = 15/3;
+          int modulo = 18 % 3;  
+          double resultado = (10*7) + (20/4); 
+          
+          System.out.println("Resultado da soma é:" +soma);
+          System.out.println("Resultado da subtração é: " +subtracao);
+          System.out.println("Resultado da multiplicação é: " +multiplicacao);
+          System.out.println("Resultado da divisão é:" +divisao);
+          System.out.println("Resultado do resto da divisão é: " +modulo);
+          System.out.println("Resultado: " +resultado);
+
+
+    }
+}
