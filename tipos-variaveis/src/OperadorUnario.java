79ed962e63fbc6ce09fa4dcff1681d9d789ac0ba
Thais-Westphal <thais.westphal12@gmail.com>
2024-06-08 22:06:25 -0300

    Exemplo de operador unário

---
+++ tipos-variaveis/src/OperadorUnario.java
@@ -0,0 +1,20 @@
+public class OperadorUnario 
+{
+    public static void main(String[] args) 
+    {
+        int x = 5;
+
+        System.out.println("Valor inicial de x: " + x);
+
+        x++; //incremento de x
+        System.out.println("Valor de x após o incremento: " + x);
+
+        int y = 10;
+
+        System.out.println("Valor inicial de y: " + y);
+
+        y--; //Decremento de y
+        System.out.println("Valor de y após o decremento: " + y);
+
+    }
+}
