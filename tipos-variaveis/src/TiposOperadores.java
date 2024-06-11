33a1f972dab54d12a31e7e0e42c9daff04a04461
Thais-Westphal <thais.westphal12@gmail.com>
2024-06-10 22:13:37 -0300

    Tipos de operadores

--- tipos-variaveis/src/TiposOperadores.java
+++ tipos-variaveis/src/TiposOperadores.java
@@ -2,6 +2,7 @@ public class TiposOperadores
 {
     public static void main(String[] args) throws Exception 
     {
+        
           double soma = 10.5 + 15.7;
           int subtracao = 113 - 25;
           int multiplicacao = 20 * 7;
