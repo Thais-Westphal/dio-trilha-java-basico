6ef3e3932b01bec136b63688429f36dcfd5c0505
Thais-Westphal <thais.westphal12@gmail.com>
2024-06-18 20:36:23 -0300

    estruturas excepcionais

---
+++ estruturas-excepcionais/Estruturas-excepcionais/FormatadorCepExemplo.java
@@ -0,0 +1,21 @@
+public class FormatadorCepExemplo 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          System.out.println("O CEP não corresponde com as regras de negocio !!!");
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }    
+}
---
+++ estruturas-excepcionais/Estruturas-excepcionais/src/ExemploExcecoes.java
@@ -0,0 +1,22 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExemploExcecoes 
+{
+      public static void main(String[] args) {
+        Number valor;
+
+        try 
+        {
+            valor = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(valor); 
+        } 
+        catch (ParseException e) 
+        {
+            e.printStackTrace();
+        }
+        
+     
+       
+    }    
+}
