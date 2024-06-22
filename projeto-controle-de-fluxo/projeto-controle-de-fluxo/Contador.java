f2d11188ea0317e0b414823ab9a1ab47fd4dd91c
Thais-Westphal <thais.westphal12@gmail.com>
2024-06-22 00:00:28 -0300

    Projeto controle de fluxo

--- projeto-controle-de-fluxo/projeto-controle-de-fluxo/Contador.java
+++ projeto-controle-de-fluxo/projeto-controle-de-fluxo/Contador.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) 
     {
-		
         Scanner terminal = new Scanner(System.in);
 
 		System.out.println("Digite o primeiro parâmetro");
