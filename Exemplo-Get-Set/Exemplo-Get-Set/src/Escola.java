165c6c299f3a59126146d5c2193101e5e197bd62
Thais-Westphal <thais.westphal12@gmail.com>
2024-06-22 22:05:05 -0300

    Exemplo de getters e setters

---
+++ Exemplo-Get-Set/Exemplo-Get-Set/src/Escola.java
@@ -0,0 +1,11 @@
+public class Escola
+{
+        public static void main(String[] args) {
+            Aluno a1 = new Aluno();
+            a1.setNome("Thais");
+            a1.setIdade(12); 
+            
+            System.out.println("A(O) aluna(o) " + a1.getNome() + " tem " + a1.getIdade() + " anos ");
+            		
+        }   
+}
